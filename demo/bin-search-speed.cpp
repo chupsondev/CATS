1901279e15e92c4acffbc2a76e0d3d0de1360a14
chupson <bratek.wojtek@gmail.com>
2023-05-11 18:22:37 +0200

    made it so cout doesnt buffer so we get real time speed of linear search

--- demo/bin-search-speed.cpp
+++ demo/bin-search-speed.cpp
@@ -39,8 +39,6 @@ int main(int argc, char *argv[]) {
         // Set console code page to UTF-8 so console known how to interpret string data
     SetConsoleOutputCP(CP_UTF8);
 
-    // Enable buffering to prevent VS from chopping up UTF-8 byte sequences
-    setvbuf(stdout, nullptr, _IOFBF, 1000);
 
 
     long long n;
