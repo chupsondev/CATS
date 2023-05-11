74ad7614953084cdd7f61b71dde92e9165a1d463
chupson <bratek.wojtek@gmail.com>
2023-05-11 17:53:39 +0200

    updated speedy bin search show program

--- demo/bin-search-speed.cpp
+++ demo/bin-search-speed.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 
-constexpr long long MAX_SEARCHED = 1e12 + 10;
 
 long long find_linear(long long n) {
     long long i = 1;
-    for(i; i <= n; i++) {
+    for (i; i <= n; i++) {
         int a = 1;
         int b = 3;
         int c = a + b;
@@ -14,43 +13,66 @@ long long find_linear(long long n) {
 
 long long bin_search(long long n, bool verbose = false) {
     long long l = 1;
-    long long r = MAX_SEARCHED;
+    long long r = n;
 
-    int i = 0;
+    long long i = 0;
 
-    while(r - l > 1) {
+    while (r - l > 1) {
         i++;
         long long mid = (r + l) / 2;
-        if(verbose) std::cout << "Zakres: " << l << "-" << r << " | Środek: " << mid << '\n'; 
-        if(n > mid) l = mid;
-        else r = mid;
+        if (verbose)
+            std::cout << "Zakres: " << l << "-" << r << " | Środek: " << mid
+                      << '\n';
+        if (n > mid)
+            l = mid;
+        else
+            r = mid;
     }
-    std::cout << "Znaleziona liczba " << r << '\n';
+    long long res = l == n ? l : r;
+    std::cout << "Znaleziona liczba " << res << '\n';
     return i;
 }
 
 int main(int argc, char *argv[]) {
-    if(argc <= 1) {
-        std::cout << "You must provide a number to be guessed" << '\n';
-        return 1;
-    } 
-
-    long long n = std::atoll(argv[1]);
-    bool verbose = false;
-    if(argc >= 2) {
-        std::string arg = argv[2];
-        if (arg == "-v") {
-            std::cout << "Verbose mode." << '\n';
-            verbose = true;
+    long long n;
+    bool verbose;
+     if (argc <= 1) {
+        std::cout << "Podaj liczbę do zgadnięcia" << '\n';
+        std::cin >> n;
+        std::cout << "Czy pokazać proces bin search? (y/n)" << '\n';
+        char c;
+        std::cin >> c;
+        verbose = c == 'y';
+    }
+    else {
+        n = std::atoll(argv[1]);
+        verbose = false;
+        if (argc > 2) {
+            std::string arg = argv[2];
+            if (arg == "-v") {
+                std::cout << "Verbose mode." << '\n';
+                verbose = true;
+            }
         }
+    }
+
+    if(n > 1e9) {
+        std::cout << "Wyszukiwanie tej liczby wolną metodą zajęło by zbyt dużo czasu. Pokażemy, że bin search i tak wyszuka ją szybko." << '\n';
+        int ops = bin_search(n, verbose);
+        std::cout << "Wykonano " << ops << " operacji." << '\n';
+        return 0;
     }
 
     std::cout << "Wyszukiwanie wolną metodą: " << '\n';
     long long operations_linear = find_linear(n);
-    std::cout << "Skończono wyszukiwać. Zajęło to w przybliżeniu " << operations_linear << " operacji" << '\n';
+    std::cout << "Skończono wyszukiwać. Zajęło to w przybliżeniu "
+              << operations_linear << " operacji" << '\n';
     std::cout << "Kliknij enter aby zacząć bin search" << '\n';
+    std::cin.clear();
+    std::cin.ignore(1);
     std::cin.get();
     std::cout << "Wyszukiwanie metodą bin search: " << '\n';
     long long operations_bs = bin_search(n, verbose);
-    std::cout << "Skończono wyszukiwać. Zajęło to w przybliżeniu " << operations_bs << " operacji" << '\n';
+    std::cout << "Skończono wyszukiwać. Zajęło to w przybliżeniu "
+              << operations_bs << " operacji" << '\n';
 }
